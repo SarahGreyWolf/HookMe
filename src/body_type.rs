b160424999b9ca011ccd6ee1107fefe1e33d2616
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2022-05-02 20:31:41 +0100

    Bot: Add working bot resending webhook

---
+++ src/body_type.rs
@@ -0,0 +1,89 @@
+use serde::{Serialize, Deserialize};
+
+pub trait Embed {
+    fn get_username(&self) -> String;
+    fn get_avatar_url(&self) -> String;
+    fn get_first_embed(&self) -> EmbedData;
+    fn get_embeds(&self) -> Vec<EmbedData>;
+}
+
+#[derive(Debug, Clone)]
+pub struct Destination {
+    pub(crate) username: String,
+    pub(crate) avatar_url: String,
+    pub(crate) server_id: u64,
+    pub(crate) channel_id: u64,
+    pub(crate) user_id: u64,
+    pub(crate) app_id: u64
+}
+
+impl Destination {
+    pub fn new(username: &str, avatar_url: &str, server_id: u64, channel_id: u64, user_id: u64, app_id: u64) -> Destination {
+        Destination {
+            username: username.into(),
+            avatar_url: avatar_url.into(),
+            server_id,
+            channel_id,
+            user_id,
+            app_id
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EmbedData {
+    pub(crate) title: String,
+    pub(crate) description: String,
+    pub(crate) url: String,
+    pub(crate) color: u32,
+    pub(crate) footer: EmbedFooter,
+    pub(crate) author: EmbedAuthor,
+    pub(crate) fields: Option<Vec<EmbedField>>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EmbedFooter {
+    pub(crate) text: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EmbedAuthor {
+    pub(crate) name: String,
+    pub(crate) url: String,
+    pub(crate) icon_url: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct EmbedField {
+    pub(crate) name: String,
+    pub(crate) value: String,
+    pub(crate) inline: Option<bool>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Discord {
+    wait: bool,
+    content: String,
+    username: String,
+    avatar_url: String,
+    tts: bool,
+    embeds: Vec<EmbedData>
+}
+
+impl Embed for Discord {
+    fn get_username(&self) -> String {
+        self.username.clone()
+    }
+
+    fn get_avatar_url(&self) -> String {
+        self.avatar_url.clone()
+    }
+
+    fn get_first_embed(&self) -> EmbedData {
+        self.embeds[0].clone()
+    }
+
+    fn get_embeds(&self) -> Vec<EmbedData> {
+        self.embeds.clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,114 @@
-use axum::{response::Html, routing::get, Router, extract::Path};
-use std::net::SocketAddr;
+use axum::{response::Html, routing::{get, post}, Router, extract::{Path, Extension, RawBody}, body::StreamBody, Json};
+use body_type::{Embed, EmbedData, Destination};
+use tower::ServiceBuilder;
+use tokio::spawn;
+use tokio::sync::{RwLock};
+use tokio::sync::mpsc::{Receiver, Sender, channel};
+use std::sync::Arc;
+use std::{net::SocketAddr, sync::atomic::Ordering};
+use serenity::{framework::standard::macros::{group}, client::{EventHandler, Context}, model::{channel::Message, gateway::Ready, id::ChannelId}};
+use serenity::async_trait;
+use serenity::builder::{CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter};
+
+
+mod body_type;
+
+#[group]
+struct Admin;
+
+struct Handler {
+    prefix: &'static str,
+    incoming_embed: Arc<RwLock<Receiver<(Destination, EmbedData)>>>,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let mut content = msg.content.clone();
+        if !content.starts_with(self.prefix) && msg.is_own(ctx) {
+            return;
+        }
+        content.remove(0);
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        let mut receiver = self.incoming_embed.write().await;
+        while let Some((dest, embed)) = receiver.recv().await {
+            for guild in &ready.guilds {
+                let guild = guild.id;
+                if dest.server_id == guild.0 {
+                    if let Ok(channels) = &guild.channels(&ctx.http).await {
+                        if let Some(channel) = channels.get(&ChannelId(dest.channel_id)) {
+                            channel.send_message(&ctx.http, |m| {
+                                m.embed(|e| {
+                                    e.title(embed.title.clone())
+                                        .author(|a| a.name(embed.author.name.clone()).icon_url(embed.author.icon_url.clone()).url(embed.author.url.clone()))
+                                        .description(embed.description.clone())
+                                        .url(embed.url.clone())
+                                        .fields({
+                                            let mut fields: Vec<(String, String, bool)> = vec![];
+                                            if let Some(e_fields) = embed.fields.clone() {
+                                                for field in e_fields {
+                                                    fields.push((field.name, field.value, field.inline.unwrap_or(false)));
+                                                }
+                                            }
+                                            fields
+                                        })
+                                        .footer(|f| f.text(embed.footer.text.clone()))
+                                })
+                            }).await.expect("Faild to send embed");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Handler {
+    pub fn new(prefix: &'static str, receiver: Receiver<(Destination, EmbedData)>) -> Handler {
+        Handler {
+            prefix,
+            incoming_embed: Arc::new(RwLock::new(receiver))
+        }
+    }
+}
+
+type SendEmbed = Arc<RwLock<Sender<(Destination, EmbedData)>>>;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/:name", get(handler));
-    let addr = SocketAddr::from(([192,168,0,42], 7777));
+    let (sender, receiver) = channel::<(Destination, EmbedData)>(2048);
+    let handler = Handler::new("`", receiver);
+
+    // Run Discord Bot
+    let discord_task = tokio::spawn(async move {
+        use serenity::prelude::*;
+        use serenity::framework::standard::{StandardFramework};
+        let framework = StandardFramework::new()
+            .configure(|c| c.prefix("`"))
+            .group(&ADMIN_GROUP);
+        let token = std::env::var("DISCORD_TOKEN").expect("Could not find Discord Token in environment");
+        let intents = GatewayIntents::non_privileged();
+        let mut client = Client::builder(token, intents)
+            .event_handler(handler)
+            .framework(framework)
+            .await
+            .expect("Error creating client");
+
+        if let Err(e) = client.start().await {
+            println!("An error occurred while running the client: {:?}", e);
+        }
+    });
+
+    let app = Router::new()
+        .route("/:server_id/:channel_id/:user_id/:app_id/discord", post(hook_discord))
+        .layer(
+            ServiceBuilder::new()
+                .layer(Extension(Arc::new(RwLock::new(sender))))
+                .into_inner(),
+        );
+    let addr = SocketAddr::from(([192,168,0,14], 80));
     println!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -12,6 +116,39 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler(Path(name): Path<String>)  -> Html<String> {
-    Html(format!("<h1>Hello, {}</h1>", name))
+// Webhook handling routes
+/// Discord webhook handling route
+async fn hook_discord(
+    Json(body): Json<body_type::Discord>,
+    Path((server_id, channel_id, user_id, app_id)): Path<(u64, u64, u64, u64)>,
+    state: Extension<SendEmbed>,
+) {
+    // println!("{:?}", body);
+    // println!("Server ID: {}", server_id);
+    // println!("Channel ID: {}", channel_id);
+    // println!("User ID: {}", user_id);
+    // println!("App ID: {}", app_id);
+    let destination = Destination::new(&body.get_username(), &body.get_avatar_url(), server_id, channel_id, user_id, app_id);
+    let lock = state.write().await;
+    lock.send((destination, body.get_first_embed())).await.expect("Failed to send embed");
+    drop(lock);
+}
+
+// Server/Guild dashboard for managing webhook settings
+// Only provides access to the server that the JWT was generated for
+async fn dashboard() {
+
+}
+
+// Post for form
+// Takes the users id e.g SarahGreyWolf#8257 and the JWT from the discord command
+async fn login() {
+}
+
+// Probably not from the web, should be called via bot command
+// Takes the users id and the server they are calling from
+// Generates a single use password/JWT using their id, the server/guild id and the current unix epoch
+// JWT is associated with the specific users discord name and id and the channel
+async fn generate_jwt() {
+
 }
